bce9941fdfa5fb8c1f8a10684dbe9bb1c928afdb
Dakota <dakota@stedi.com>
2020-12-02 09:08:26 -0500

    fix typo

--- index.ts
+++ index.ts
@@ -8,9 +8,9 @@ import * as day2prob2 from './src/day1.2';
 
 async function solveAll(){
    const day1answer1 = await day1prob1.answer()
-   console.log('day1 answer is ' + day1answer1)
+   console.log('day1 answer1 is ' + day1answer1)
    const day1answer2 = await day2prob2.answer()
-   console.log('day2 answer is ' + day1answer2)
+   console.log('day1 answer2 is ' + day1answer2)
 }
 
 solveAll()
