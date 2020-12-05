ea7c7455678a2d9df0dadc892eeadd7ecd38cced
Dakota <dakota@stedi.com>
2020-12-05 18:07:20 -0500

    added day 3

--- index.ts
+++ index.ts
@@ -2,22 +2,40 @@ import * as day1prob1 from './src/day1.1';
 import * as day1prob2 from './src/day1.2';
 import * as day2prob1 from './src/day2.1';
 import * as day2prob2 from './src/day2.2';
+import * as day3prob1 from './src/day3.1';
+import * as day3prob2 from './src/day3.2';
 
 
 //const answerday1 = day1prob.answer()
 //day1prob.answer().then(function (result){console.log('Problem 1 answer is: ' + result)})
 
+let testString = `..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#
+`
 
 async function solveAll(){
    const day1answer1 = await day1prob1.answer()
    console.log('day1 answer1 is ' + day1answer1)
    const day1answer2 = await day1prob2.answer()
-   console.log('day1 answer2 is ' + day1answer2)
+   console.log('da3y1 answer2 is ' + day1answer2)
    const day2answer1 = await day2prob1.answer()
    console.log('day2 answer1 is ' + day2answer1)
    const day2answer2 = await day2prob2.answer()
    console.log('day2 answer2 is ' + day2answer2)
-   
+   const test = new day3prob1.Map(testString) //Map(MapData:testString)
+   const day3answer1 = await day3prob1.answer()
+   console.log(day3answer1)
+   const day3answer2 = await day3prob2.answer()
+   console.log(day3answer2)
 }
 
 //day2prob1.answer()
---
+++ src/day3.1.ts
@@ -0,0 +1,143 @@
+import axios, { AxiosResponse } from 'axios'
+import { exception } from 'console';
+import { stringify } from 'querystring';
+
+
+
+const inputUrl = 'https://adventofcode.com/2020/day/3/input'
+let session = process.env.github_session
+
+interface Coord {
+    hasTree : boolean
+}
+
+interface PlayerPos {
+    x: number,
+    y: number
+}
+
+// 0 [ 0 1 2 3 4 5 ]
+// 1 [ 0 1 2 3 4 5 ]
+// 2 [ 0 1 2 3 4 5 ]
+
+export class Map {
+    rawMapData: string;
+    parsedMapData: Coord[][] = []
+    currentPlayerPos: PlayerPos = {x: 0, y: 0}
+    
+    // the farthest a player can travel right before reset
+    xbounds: number
+
+    // the farthest a play can travel down before end
+    ybounds: number
+
+    constructor(mapData: string) {
+        this.rawMapData = mapData;
+        let splitMapData = this.rawMapData.split('\n')
+        let numXPos = splitMapData[0].length
+        this.xbounds = numXPos - 1
+        this.ybounds = splitMapData.length -1
+
+        
+        for (let step = 0; step < numXPos; step++){
+            this.parsedMapData.push(new Array)
+        }
+        //adding the arrays for x coordinates
+
+        for (let yRow in splitMapData) {
+            const charArray = splitMapData[yRow].split('')
+            for (let xChar in charArray){
+                //let coord
+                switch(charArray[xChar]){
+                    case '.':
+                        //let coord : Coord = {hasTree: false}
+                        this.parsedMapData[xChar].push({hasTree: false})
+                        break;
+                    case '#':
+                        //let coord
+                        this.parsedMapData[xChar].push({hasTree: true})
+                        break;
+                    default:
+                        console.log(charArray[xChar])
+                        exception('unsupported char in provided map')
+                        break;
+                }
+                //this.parsedMapData[xChar].push(charArray[xChar])
+            }
+            //switch (splitMapData[yRow])
+        }
+
+    }
+    moveRight(increment: number){
+        
+        //where we plan to be
+        let sum = this.currentPlayerPos.x + increment
+        
+        //check if it puts us out of bounds
+        if (sum > this.xbounds ) {
+            //console.debug('player went out of bounds - resetting x')
+            //console.debug('sum: ', sum)
+            let diff = sum - this.xbounds
+            //console.debug('diff: ', diff)
+            //let remainder = increment - diff
+            //console.debug('remainder: ', remainder)
+            this.currentPlayerPos.x = diff - 1
+            return
+        }
+        else {
+            this.currentPlayerPos.x = this.currentPlayerPos.x + increment
+            return
+        }
+        
+        //this.currentPlayerPos.x = this.currentPlayerPos.x + increment
+    }
+    moveDown(increment: number){
+        
+        //where we plan to be
+        let sum = this.currentPlayerPos.y + increment
+        if (sum > this.ybounds - 1){
+            console.debug('end of map reached')
+            exception('end of map reached')
+            
+        }
+        else {
+            this.currentPlayerPos.y = sum
+        }
+        //this.currentPlayerPos.y = this.currentPlayerPos.y + increment
+
+        
+    }
+    standingOnTree(){
+        const status: boolean = this.parsedMapData[this.currentPlayerPos.x][this.currentPlayerPos.y].hasTree
+        return status
+    }
+
+}
+
+export async function answer(){
+    const input = await axios.get(inputUrl, {headers: {cookie:'session='+session}, withCredentials: true})
+    //console.log(input.data)
+    const map = new Map(input.data)
+    let treeCounter: number = 0
+    console.log(map.ybounds)
+    console.log(map.parsedMapData[0].length)
+
+    for (let step = 0; step < map.ybounds-1; step++){
+        map.moveRight(3)
+        map.moveDown(1)
+        if (map.standingOnTree() == true ){
+            treeCounter++
+        }
+    }
+    // for (let step in map.parsedMapData[0]) {
+    //     console.log('iter')
+    //     map.moveRight(3)
+    //     map.moveDown(1)
+    //     if (map.standingOnTree() == true ){
+    //         treeCounter++
+    //     }
+        
+    // }
+    return treeCounter
+}
+
